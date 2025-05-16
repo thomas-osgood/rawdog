b1502f20e35d2fc42e8f08325dba4faadc1e08cf
thomas-osgood <thomas-osgood@users.noreply.github.com>
2025-05-15 23:23:54 -0400

    added ERR_ENDPOINT_MAP_SET

--- server/internal/messages/errors.go
+++ server/internal/messages/errors.go
@@ -2,6 +2,7 @@ package messages
 
 const ERR_CONN_ACCEPT string = "ERROR Accepting Conn: %s\n"
 const ERR_DATA_READ string = "Error Reading Data: %s\n"
+const ERR_ENDPOINT_MAP_SET string = "endpoint map already set"
 const ERR_ENDPOINT_UNKNOWN string = "unknown endpoint \"%d\""
 const ERR_IEH_SET string = "can only set one InvalidEndpointHandler"
 const ERR_LISTEN_EMPTY string = "listenaddress cannot be an empty string"
