d5a0203b7dcccafdd78b983056e80d9b17cd31d6
thomas-osgood <thomas-osgood@users.noreply.github.com>
2025-05-17 17:06:45 -0400

    updated consts

--- comms/internal/constants/consts.go
+++ comms/internal/constants/consts.go
@@ -1,19 +1,17 @@
 package constants
 
-import "math"
-
 // helper constant defining a nullbyte
 const NULL_BYTE string = "\x00"
 
 // size of the byte block that will be
 // used to read data.
-const SZ_DATABUFF uint64 = 2048
+const SZ_DATABUFF uint64 = (1 << 11)
 
 // maximum size of the metadata block.
-const SZ_METADATA_MAX uint16 = math.MaxUint16
+const SZ_METADATA_MAX uint16 = (1 << 16) - 1
 
 // size of the data size block in the transmission.
-const SZ_SIZEBLOCK_DAT uint64 = 8
+const SZ_SIZEBLOCK_DAT uint64 = (1 << 3)
 
 // size of the metadata size block in the transmission.
-const SZ_SIZEBLOCK_MD uint64 = 2
+const SZ_SIZEBLOCK_MD uint64 = (1 << 1)
