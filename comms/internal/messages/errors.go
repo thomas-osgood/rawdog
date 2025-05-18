a54a6e348151345e53385af57979adfcc5179d48
thomas-osgood <thomas-osgood@users.noreply.github.com>
2025-05-17 20:36:22 -0400

    added ERR_SEND_TIMEOUT

--- comms/internal/messages/errors.go
+++ comms/internal/messages/errors.go
@@ -2,3 +2,4 @@ package messages
 
 const ERR_MD_LARGE string = "metadata size of %d bytes is too large. max size is %d bytes"
 const ERR_READ_TIMEOUT string = "read transmission timeout"
+const ERR_SEND_TIMEOUT string = "read transmission timeout"
